3c553cdf8523e309278b38afd7a0804cb6a21087
atlasgong <68199735+atlasgong@users.noreply.github.com>
2024-11-23 23:14:19 -0500

    content: add mental wellness section to home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
     site: 'https://mindvista.ca',
-    integrations: [tailwind(), react()],
+    integrations: [tailwind(), react(), icon()],
 });
