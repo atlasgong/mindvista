07ff0989bd7308ea93b7c9082e235b1da53eec7e
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2024-12-25 21:42:38 -0600

    content: change crisis button text

--- src/components/buttons/EmergencyButton.tsx
+++ src/components/buttons/EmergencyButton.tsx
@@ -4,12 +4,8 @@ interface EmergencyButtonProps {
 
 export default function EmergencyButton(props: EmergencyButtonProps) {
     return (
-        <a
-            href="/im-in-crisis"
-            className={`${props.className} w-fit rounded-lg border-2 border-solid border-cText bg-cRed p-2 font-bold text-cSoftWhite hover:opacity-90 cursor-pointer`}
-            role="button"
-        >
-            Is this an emergency?
+        <a href="/im-in-crisis" className={`${props.className} w-fit cursor-pointer rounded-lg border-2 border-solid border-cText bg-cRed p-2 font-bold text-cSoftWhite hover:opacity-90`} role="button">
+            Need help NOW?
         </a>
     );
 }
