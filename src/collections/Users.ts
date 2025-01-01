28120a7faf5e63e1249829ff2b13c1eb20aa36f1
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2025-01-01 17:00:02 -0500

    build: configure payload users collection

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,13 +1,24 @@
-import type { CollectionConfig } from 'payload'
+import type { CollectionConfig } from "payload";
 
 export const Users: CollectionConfig = {
-  slug: 'users',
-  admin: {
-    useAsTitle: 'email',
-  },
-  auth: true,
-  fields: [
-    // Email added by default
-    // Add more fields as needed
-  ],
-}
+    slug: "users",
+    admin: {
+        useAsTitle: "email",
+    },
+    auth: true,
+    fields: [
+        // Email added by default
+        // Add more fields as needed
+        {
+            name: "fullName",
+            required: true,
+            type: "text",
+            validate: (val: string | null | undefined) => {
+                if (!val) return "Must enter full name.";
+                if (val.length < 2) return "Full name must 2 characters or more.";
+                if (val.length > 32) return "Full name must be 32 characters or fewer.";
+                return true;
+            },
+        },
+    ],
+};
