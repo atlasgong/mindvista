942d5303f9f4a92312fbe2372f9943009d56734b
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2025-01-01 18:23:57 -0500

    ci: configure tailwind and add global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 // https://tailwindcss.com/docs/configuration
 
 export default {
-  content: [
-        './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+    content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+    darkMode: "selector",
+    theme: {
+        fontFamily: {
+            sans: ["Inter Variable", ...fontFamily.sans],
+            serif: ["Libre Baskerville", "Baskerville", ...fontFamily.serif],
+        },
+        extend: {
+            colors: {
+                cBackground: "var(--background)",
+                cBackgroundOffset: "var(--background-offset)",
+                cBackgroundOffsetAccent: "var(--background-offset-accent)",
+                cAccent: "var(--accent)",
+                cText: "var(--text)",
+                cTextOffset: "var(--text-offset)",
+                cRed: "var(--red)",
+                cBorder: "var(--border)",
+                cSoftWhite: "var(--soft-white)",
+                cNavyBlue: "var(--navy-blue)",
+                cLightBlue: "var(--light-blue)",
+                cPurple: "var(--purple)",
+            },
+        },
+    },
+    plugins: [],
+};
