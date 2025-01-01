76c59c64c27fbe9f46ab7fc8aca446ebf79628ee
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2025-01-01 15:38:06 -0500

    build: add tailwind css

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+// this file should not need to be modified
+// see https://nextjs.org/docs/app/building-your-application/styling/tailwind-css
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+// https://tailwindcss.com/docs/configuration
+
+export default {
+  content: [
+        './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
