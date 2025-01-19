33d14327ab7303571debfe2c8aa43a318ae2c1e6
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2025-01-19 18:45:17 -0500

    ui: change favicon

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -15,7 +15,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="en" suppressHydrationWarning>
             <head>
                 <meta charSet="utf-8" />
-                <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 {/* social media here */}
                 <script
