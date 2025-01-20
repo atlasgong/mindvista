f69f18be586d05a6f75712ddbf7891528a9df477
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2025-01-20 14:08:48 -0500

    content: remove team member from abt pg
    refactor to use flexbox instead of grid for more flexible adjustment

--- src/app/(app)/(pages)/about/components/TeamSection.tsx
+++ src/app/(app)/(pages)/about/components/TeamSection.tsx
@@ -15,18 +15,18 @@ interface Props {
 export default function TeamSection(props: Props) {
     return (
         <section className="mb-16">
-            <h2 className="text-cText mb-8 text-center text-3xl font-bold">{props.title}</h2>
-            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            <h2 className="mb-8 text-center text-3xl font-bold text-cText">{props.title}</h2>
+            <div className="flex flex-wrap justify-center gap-8">
                 {props.members.map((member) => (
-                    <div key={member.name} className="bg-cBackgroundOffset flex flex-col items-center rounded-xl p-6 text-center shadow-lg">
+                    <div key={member.name} className="flex w-full flex-col items-center rounded-xl bg-cBackgroundOffset p-6 text-center shadow-lg sm:w-[calc(50%-1rem)] lg:w-[calc(33.333%-1.34rem)]">
                         <div className="mb-4 h-40 w-40 overflow-hidden rounded-full">
                             <Image src={member.image} alt={member.name} width={500} height={500} className="h-full w-full object-cover" />
                         </div>
                         <div className="flex flex-row items-center justify-center">
-                            <h3 className="text-cText text-xl font-semibold">{member.name}</h3>
-                            {member.pronouns !== "" && <span className="text-cTextOffset ml-1 text-sm italic">({member.pronouns})</span>}
+                            <h3 className="text-xl font-semibold text-cText">{member.name}</h3>
+                            {member.pronouns !== "" && <span className="ml-1 text-sm italic text-cTextOffset">({member.pronouns})</span>}
                         </div>
-                        <p className="text-cTextOffset font-medium">{member.role}</p>
+                        <p className="font-medium text-cTextOffset">{member.role}</p>
                     </div>
                 ))}
             </div>
