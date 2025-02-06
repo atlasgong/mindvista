288dc9ed7712221ad006abde27cbe183e92ccf61
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2025-02-06 15:16:21 -0500

    refactor: remove completed migrations

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250206_053130_localize_events from "./20250206_053130_localize_events";
-
-export const migrations = [
-    {
-        up: migration_20250206_053130_localize_events.up,
-        down: migration_20250206_053130_localize_events.down,
-        name: "20250206_053130_localize_events",
-    },
-];
