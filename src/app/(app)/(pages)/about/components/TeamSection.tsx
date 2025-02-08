6905b6f078a4756763b710d6cb0653f94c48bca2
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2025-02-07 23:49:36 -0500

    refactor: explicitly define image and pronouns to be optional

--- src/app/(app)/(pages)/about/components/TeamSection.tsx
+++ src/app/(app)/(pages)/about/components/TeamSection.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 interface TeamMember {
     role: string;
     name: string;
-    pronouns: string;
-    image: string;
+    pronouns?: string;
+    image?: string;
 }
 
 interface Props {
@@ -28,11 +28,11 @@ export default function TeamSection(props: Props) {
                 {props.members.map((member) => (
                     <div key={member.name} className={`flex flex-col items-center rounded-xl bg-cBackgroundOffset p-6 text-center shadow-lg dark:bg-mindvista-950 ${isMarketingTeam ? "w-full" : "w-full sm:w-[calc(50%-1rem)] lg:w-[calc(33.333%-1.34rem)]"}`}>
                         <div className="mb-4 h-40 w-40 overflow-hidden rounded-full">
-                            <Image src={member.image} alt={member.name} width={500} height={500} className="h-full w-full object-cover" />
+                            <Image src={member.image || "/team/avatarPlaceholder.png"} alt={member.name} width={500} height={500} className="h-full w-full object-cover" />
                         </div>
                         <div className="flex flex-row items-center justify-center">
                             <h3 className="text-xl font-semibold text-cText">{member.name}</h3>
-                            {member.pronouns !== "" && <span className="ml-1 text-sm italic text-cTextOffset">({member.pronouns})</span>}
+                            {member.pronouns && <span className="ml-1 text-sm italic text-cTextOffset">({member.pronouns})</span>}
                         </div>
                         <p className="font-medium text-cTextOffset">{member.role}</p>
                     </div>
--- src/app/(app)/(pages)/about/page.tsx
+++ src/app/(app)/(pages)/about/page.tsx
@@ -15,7 +15,7 @@ const teams = {
     ],
     marketing: [
         { role: "Social Media Advisor", name: "Abbie Carnahan", pronouns: "she/her", image: "/team/abbie.webp" },
-        { role: "Social Media Coordinator", name: "Amanda Borja", pronouns: "she/her", image: "/team/avatarPlaceholder.png" },
+        { role: "Social Media Coordinator", name: "Amanda Borja", pronouns: "she/her" },
         { role: "Marketing & Outreach Coordinator", name: "Paige Metcalf", pronouns: "she/her", image: "/team/paige.webp" },
         { role: "TikTok & Video Content Creator", name: "Naomi Harmel", pronouns: "she/her", image: "/team/naomi.webp" },
     ],
