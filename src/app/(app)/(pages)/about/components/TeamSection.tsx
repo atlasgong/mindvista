acad56a829ddf1fbb4c56ea258f0781d8949bd8f
novruzoff <murad.novruzov1899@gmail.com>
2025-02-08 00:01:21 -0500

    Revert "fix(ui): about page marketing section, grid changed to 2x2"
    This reverts commit 17eb141f9f90200a0d39c40fa7ef0dc967367882.

--- src/app/(app)/(pages)/about/components/TeamSection.tsx
+++ src/app/(app)/(pages)/about/components/TeamSection.tsx
@@ -13,20 +13,12 @@ interface Props {
 }
 
 export default function TeamSection(props: Props) {
-    const isMarketingTeam = props.title === "Marketing + Social Media Team";
-
     return (
         <section className="mb-16">
             <h2 className="mb-8 text-center text-3xl font-bold text-cText">{props.title}</h2>
-            <div
-                className={`${
-                    isMarketingTeam
-                        ? "grid grid-cols-1 gap-8 sm:grid-cols-2" // 2x2 grid for Marketing Team
-                        : "flex flex-wrap justify-center gap-8" // Default flex for others
-                }`}
-            >
+            <div className="flex flex-wrap justify-center gap-8">
                 {props.members.map((member) => (
-                    <div key={member.name} className={`flex flex-col items-center rounded-xl bg-cBackgroundOffset p-6 text-center shadow-lg dark:bg-mindvista-950 ${isMarketingTeam ? "w-full" : "w-full sm:w-[calc(50%-1rem)] lg:w-[calc(33.333%-1.34rem)]"}`}>
+                    <div key={member.name} className="flex w-full flex-col items-center rounded-xl bg-cBackgroundOffset p-6 text-center shadow-lg sm:w-[calc(50%-1rem)] lg:w-[calc(33.333%-1.34rem)] dark:bg-mindvista-950">
                         <div className="mb-4 h-40 w-40 overflow-hidden rounded-full">
                             <Image src={member.image || "/team/avatarPlaceholder.png"} alt={member.name} width={500} height={500} className="h-full w-full object-cover" />
                         </div>
