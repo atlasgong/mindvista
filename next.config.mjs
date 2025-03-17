eb7c0e87e04713dbfcb12ce11b089331f6e2893c
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2025-03-17 01:10:42 -0400

    build: configure nextjs remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,48 @@
-import { withPayload } from '@payloadcms/next/withPayload'
+import { withPayload } from "@payloadcms/next/withPayload";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Your Next.js config here
-}
 
-export default withPayload(nextConfig)
+const isDevelopment = process.env.APP_ENV === "development";
+const isProduction = process.env.APP_ENV === "production";
+const isStaging = process.env.APP_ENV === "staging";
+
+// parse NEXT_PUBLIC_SERVER_URL for local development
+const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL || "localhost:3000";
+const [localHostname, localPort] = serverUrl.replace(/^https?:\/\//, "").split(":");
+
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            ...(isProduction
+                ? [
+                      {
+                          protocol: "https",
+                          hostname: "mindvista.ca",
+                          pathname: "/api/media/**",
+                      },
+                  ]
+                : []),
+            ...(isStaging
+                ? [
+                      {
+                          protocol: "https",
+                          hostname: "mindvista-[a-z0-9]{9}-agvq.vercel.app",
+                          pathname: "/api/media/**",
+                      },
+                  ]
+                : []),
+            ...(isDevelopment
+                ? [
+                      {
+                          protocol: "http",
+                          hostname: localHostname,
+                          ...(localPort && { port: localPort }),
+                          pathname: "/api/media/**",
+                      },
+                  ]
+                : []),
+        ],
+    },
+};
+
+export default withPayload(nextConfig);
