e0320f4f7d811be2dbb05f9686776ee2216f4a9f
novruzoff <murad.novruzov1899@gmail.com>
2025-03-22 23:28:44 -0400

    feat(cms): migration created for about page

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250228_031847_payload_about from './20250228_031847_payload_about';
+import * as migration_20250323_024739_payload_about from './20250323_024739_payload_about';
 
 export const migrations = [
   {
     up: migration_20250228_031847_payload_about.up,
     down: migration_20250228_031847_payload_about.down,
-    name: '20250228_031847_payload_about'
+    name: '20250228_031847_payload_about',
+  },
+  {
+    up: migration_20250323_024739_payload_about.up,
+    down: migration_20250323_024739_payload_about.down,
+    name: '20250323_024739_payload_about'
   },
 ];
