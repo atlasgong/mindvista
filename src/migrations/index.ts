21780f2371d42a3702bc0b90e2726b4d4c658a6f
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2025-04-02 18:36:15 -0400

    cms: add migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250402_215618_volunteer_pg from "./20250402_215618_volunteer_pg";
+import * as migration_20250402_223601_rootuser from "./20250402_223601_rootuser";
 
 export const migrations = [
     {
         up: migration_20250402_215618_volunteer_pg.up,
         down: migration_20250402_215618_volunteer_pg.down,
         name: "20250402_215618_volunteer_pg",
+    },
+    {
+        up: migration_20250402_223601_rootuser.up,
+        down: migration_20250402_223601_rootuser.down,
+        name: "20250402_223601_rootuser",
     },
 ];
