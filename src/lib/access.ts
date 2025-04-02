3f558e9de2cba72167de91797e2cb4bcd200f79a
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2025-04-02 19:48:15 -0400

    fix: root user should have admin privileges

--- src/lib/access.ts
+++ src/lib/access.ts
@@ -3,13 +3,12 @@ import type { User } from "@/payload-types";
 
 export const denyAccess: Access = ({ req }) => {
     const user = req.user as User | null;
-    if (!user) return false;
-    return user.role === "root";
+    return user?.role === "root";
 };
 
 export const isAdmin: FieldAccess = ({ req }) => {
     const user = req.user as User | null;
-    return user?.role === "admin";
+    return user?.role === "root" || user?.role === "admin";
 };
 
 export const isContentEditor: FieldAccess = ({ req }) => {
@@ -26,20 +25,17 @@ export const isContentEditorFr: FieldAccess = ({ req }) => {
 
 export const denyAccessField: FieldAccess = ({ req }) => {
     const user = req.user as User | null;
-    if (!user) return false;
-    return user.role === "root";
+    return user?.role === "root";
 };
 
 // only admin and contentEditor can edit content
 export const canEditContent: FieldAccess = ({ req }) => {
     const user = req.user as User | null;
-    if (!user) return false;
-    return user.role === "admin" || user.role === "contentEditor";
+    return user?.role === "admin" || user?.role === "contentEditor";
 };
 
 // only admin and contentEditorFr can edit French content
 export const canEditFrenchContent: FieldAccess = ({ req }) => {
     const user = req.user as User | null;
-    if (!user) return false;
-    return user.role === "admin" || user.role === "contentEditorFr";
+    return user?.role === "admin" || user?.role === "contentEditorFr";
 };
