d9dfe5d7602ded6b3a4b15ddf6ab04db892fa45f
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2025-04-02 19:52:15 -0400

    feat(cms): add root user (#154)
    * cms: denyAccess and denyAccessField should allow bypass for root user
    
    * cms: add migration

--- src/lib/access.ts
+++ src/lib/access.ts
@@ -1,13 +1,14 @@
 import type { FieldAccess, Access } from "payload";
 import type { User } from "@/payload-types";
 
-export const denyAccess: Access = () => {
-    return false;
+export const denyAccess: Access = ({ req }) => {
+    const user = req.user as User | null;
+    return user?.role === "root";
 };
 
 export const isAdmin: FieldAccess = ({ req }) => {
     const user = req.user as User | null;
-    return user?.role === "admin";
+    return user?.role === "root" || user?.role === "admin";
 };
 
 export const isContentEditor: FieldAccess = ({ req }) => {
@@ -22,20 +23,19 @@ export const isContentEditorFr: FieldAccess = ({ req }) => {
 
 // PERMISSIONS START BELOW ------------
 
-export const denyAccessField: FieldAccess = () => {
-    return false;
+export const denyAccessField: FieldAccess = ({ req }) => {
+    const user = req.user as User | null;
+    return user?.role === "root";
 };
 
 // only admin and contentEditor can edit content
 export const canEditContent: FieldAccess = ({ req }) => {
     const user = req.user as User | null;
-    if (!user) return false;
-    return user.role === "admin" || user.role === "contentEditor";
+    return user?.role === "admin" || user?.role === "contentEditor";
 };
 
 // only admin and contentEditorFr can edit French content
 export const canEditFrenchContent: FieldAccess = ({ req }) => {
     const user = req.user as User | null;
-    if (!user) return false;
-    return user.role === "admin" || user.role === "contentEditorFr";
+    return user?.role === "admin" || user?.role === "contentEditorFr";
 };
