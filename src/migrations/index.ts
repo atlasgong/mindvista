5ff00d020ea1cf2ac9178cf67880b07aa4e65ea9
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2025-05-25 14:39:07 -0400

    cms: create migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20250403_154007_remove_root from "./20250403_154007_remove_root";
 import * as migration_20250430_051055_sentinel from "./20250430_051055_sentinel";
+import * as migration_20250525_183845_about_pg from "./20250525_183845_about_pg";
 
 export const migrations = [
     {
@@ -11,5 +12,10 @@ export const migrations = [
         up: migration_20250430_051055_sentinel.up,
         down: migration_20250430_051055_sentinel.down,
         name: "20250430_051055_sentinel",
+    },
+    {
+        up: migration_20250525_183845_about_pg.up,
+        down: migration_20250525_183845_about_pg.down,
+        name: "20250525_183845_about_pg",
     },
 ];
